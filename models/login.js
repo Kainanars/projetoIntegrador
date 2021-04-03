1c159155b45387e926b2e7ca4e85c5b573a7936b
Andreza Campbell <campbell.andreza@gmail.com>
2021-04-03 15:06:38 -0400

    colocando espacamentos na tela de cadastro de produto

--- models/login.js
+++ models/login.js
@@ -1,20 +1,18 @@
-const users = [
-    {
-        login: 'admin', 
-        password: 'admin123'
-    } , {
-        login: 'manager', 
-        password: 'manager123'
-    }
-]
+const users = [{
+    login: 'admin',
+    password: 'admin123'
+}, {
+    login: 'manager',
+    password: 'manager123'
+}]
 
 function authenticateUser(login, password) {
-    const user = users.find(function (user){
+    const user = users.find(function(user) {
         return user.login === login && user.password === password;
-    }); 
+    });
     return user
 }
 
 module.exports = {
-    authenticateUser:authenticateUser, 
+    authenticateUser: authenticateUser,
 }
