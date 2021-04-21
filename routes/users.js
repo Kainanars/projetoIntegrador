3f521975c603dd5632ddef36f6e64820bc398cf2
NehamaMandelbaum <nehama1836@gmail.com>
2021-04-21 11:23:15 -0300

    Primeiro rascunho da página de cadastro de usuários

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+// Cadastrar novos usuários
+// /users/cadastro
+router.get('/cadastro', function(req, res){
+  res.render('cadastroUsuarios')
+})
+
 module.exports = router;
